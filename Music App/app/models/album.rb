90de39f3eca690302c3ea62159dfccea0b138ab5
Jeffrey Cheng <JeffreyCheng92@gmail.com>
2015-07-16 12:57:42 -0700

    validations for band album tracks

--- Music App/app/models/album.rb
+++ Music App/app/models/album.rb
@@ -1,2 +1,3 @@
 class Album < ActiveRecord::Base
+  validates :band_id, :title, presence: true
 end
--- Music App/app/models/track.rb
+++ Music App/app/models/track.rb
@@ -1,2 +1,3 @@
 class Track < ActiveRecord::Base
+  validates :album_id, :name, presence: true
 end

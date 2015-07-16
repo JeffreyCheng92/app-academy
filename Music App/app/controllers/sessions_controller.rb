42991486606e332a8f420902f6c75b600427d07d
Jeffrey Cheng <JeffreyCheng92@gmail.com>
2015-07-16 11:20:47 -0700

    add session controller/route

--- Music App/app/controllers/sessions_controller.rb
+++ Music App/app/controllers/sessions_controller.rb
@@ -1,2 +1,17 @@
 class SessionsController < ApplicationController
+
+  def new
+    render :new
+  end
+
+  def create
+    current_user.reset_session_token
+    session[:session_token] = current_user[:session_token]
+  end
+
+  def destroy
+    current_user.reset_session_token
+    session[session_token] = nil
+  end
+
 end

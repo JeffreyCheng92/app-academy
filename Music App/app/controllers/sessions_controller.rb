0ed866ead298aca28424f7abaf243e60b571a07b
Jeffrey Cheng <JeffreyCheng92@gmail.com>
2015-07-16 17:11:15 -0700

    add edit form for track

--- Music App/app/controllers/sessions_controller.rb
+++ Music App/app/controllers/sessions_controller.rb
@@ -6,8 +6,8 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by_credentials(params[:user][:email],
                                     params[:user][:password])
-    log_in!(user)          
-    redirect_to user_url(user)
+    log_in!(user)
+    redirect_to bands_url
   end
 
   def destroy

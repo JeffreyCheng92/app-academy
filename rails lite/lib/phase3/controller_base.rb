46c40546234269bdc0a35af71d3be84df05259e8
Jeffrey Cheng <JeffreyCheng92@gmail.com>
2015-07-21 14:19:33 -0700

    added a render content method

--- rails lite/lib/phase3/controller_base.rb
+++ rails lite/lib/phase3/controller_base.rb
@@ -8,6 +8,10 @@ module Phase3
     # use ERB and binding to evaluate templates
     # pass the rendered html to render_content
     def render(template_name)
+      file_name = "views/#{self.class.to_s.underscore}/#{template_name.to_s}.html.erb"
+      doc = File.read(file_name)
+      doc_erb = ERB.new(doc).result(binding)
+      render_content(doc_erb, 'text/html')
     end
   end
 end

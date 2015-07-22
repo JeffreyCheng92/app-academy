9e8fd645104f84a54a783d9986cc43b1887d2897
Jeffrey Cheng <JeffreyCheng92@gmail.com>
2015-07-22 10:07:48 -0700

    added a nested params method to break up params into hash

--- rails lite/lib/phase5/params.rb
+++ rails lite/lib/phase5/params.rb
@@ -1,4 +1,5 @@
 require 'uri'
+require 'byebug'
 
 module Phase5
   class Params
@@ -10,9 +11,13 @@ module Phase5
     # You haven't done routing yet; but assume route params will be
     # passed in as a hash to `Params.new` as below:
     def initialize(req, route_params = {})
+      @params = route_params
+      parse_www_encoded_form(req.query_string) if req.query_string
+      parse_www_encoded_form(req.body) if req.body
     end
 
     def [](key)
+      @params[key.to_s] || @params[key.intern]
     end
 
     # this will be useful if we want to `puts params` in the server log
@@ -29,11 +34,45 @@ module Phase5
     # should return
     # { "user" => { "address" => { "street" => "main", "zip" => "89436" } } }
     def parse_www_encoded_form(www_encoded_form)
+      query_array = URI::decode_www_form(www_encoded_form)
+
+      query_array.each do |long_key, value|
+        scope = @params
+        keys = parse_key(long_key)
+
+        keys.each_with_index do |key, idx|
+          if keys.count - 1 == idx
+            scope[key] = value
+          else
+            scope[key] ||= {}
+            scope = scope[key]
+          end
+        end
+      end
+
+      # URI.decode_www_form(www_encoded_form, enc=Encoding::UTF_8).each do |k,v|
+      #   parsed_keys = parse_key(k)
+        # Eric says dont use deep merge
+        # if parsed_keys.count > 1
+        #   hash = { parsed_keys.pop => v }
+        #   until parsed_keys.empty?
+        #     if hash.keys.include?(parsed_keys.last)
+        #       hash[parsed_keys.pop].merge!(hash)
+        #     else
+        #       hash = { parsed_keys.pop => hash }
+        #     end
+        #   end
+        #   @params.deep_merge!(hash)
+        # else
+        #   @params[k] = v
+        # end
+      # end
     end
 
     # this should return an array
     # user[address][street] should return ['user', 'address', 'street']
     def parse_key(key)
+      key.split(/\]\[|\[|\]/)
     end
   end
 end
